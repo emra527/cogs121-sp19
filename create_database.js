ba0ce16d8eec502e3a239c457b4dae22f0a29d72
matianyu2004 <35642292+matianyu2004@users.noreply.github.com>
2019-04-30 12:26:49 -0700

    Add files via upload
    backend update

---
+++ create_database.js
@@ -0,0 +1,24 @@
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database('passwords.db');
+
+// run each database statement *serially* one after another
+// (if you don't do this, then all statements will run in parallel,
+//  which we don't want)
+db.serialize(() => {
+  // create a new database table:
+  db.run("CREATE TABLE users_to_passwords (name TEXT, password TEXT)");
+
+  // insert 3 rows of data:
+  db.run("INSERT INTO users_to_passwords VALUES ('tim', '007')");
+  //db.run("INSERT INTO users_to_pets VALUES ('John', 'student', 'dog.jpg')");
+  //db.run("INSERT INTO users_to_pets VALUES ('Carol', 'engineer', 'bear.jpg')");
+
+  console.log('successfully created the users_to_passwords table in passwords.db');
+
+  // print them out to confirm their contents:
+  db.each("SELECT name, password FROM users_to_passwords", (err, row) => {
+      console.log(row.name + ": " + row.password);
+  });
+});
+
+db.close();
---
+++ server.js
@@ -0,0 +1,86 @@
+
+const express = require('express');
+const app = express();
+
+
+// use this library to interface with SQLite databases: https://github.com/mapbox/node-sqlite3
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database('passwords.db');
+
+app.use(express.static('static_files'));
+
+
+app.get('/users', (req, res) => {
+  // db.all() fetches all results from an SQL query into the 'rows' variable:
+  db.all('SELECT name FROM users_to_passwords', (err, rows) => {
+    console.log(rows);
+    const allUsernames = rows.map(e => e.name);
+    console.log(allUsernames);
+    res.send(allUsernames);
+  });
+});
+
+
+// POST data about a user to insert into the database
+// (note that this will insert duplicate entries!)
+//
+// To test, use the web frontend interface at:
+//   http://localhost:3000/petsapp.html
+// use this library to parse HTTP POST requests
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true})); // hook up with your app
+app.post('/users', (req, res) => {
+  console.log(req.body);
+
+  db.run(
+    'INSERT INTO users_to_passwords VALUES ($name, $password)',
+    // parameters to SQL query:
+    {
+      $name: req.body.name,
+      $password: req.body.password,
+    },
+    // callback function to run when the query finishes:
+    (err) => {
+      if (err) {
+        res.send({message: 'error in app.post(/users)'});
+      } else {
+        res.send({message: 'successfully run app.post(/users)'});
+      }
+    }
+  );
+});
+
+
+// GET profile data for a user
+//
+// To test, open these URLs in your browser:
+//   http://localhost:3000/users/Philip
+//   http://localhost:3000/users/Carol
+//   http://localhost:3000/users/invalidusername
+app.get('/users/:userid', (req, res) => {
+  const nameToLookup = req.params.userid; // matches ':userid' above
+
+  // db.all() fetches all results from an SQL query into the 'rows' variable:
+  db.all(
+    'SELECT * FROM users_to_passwords WHERE name=$name',
+    // parameters to SQL query:
+    {
+      $name: nameToLookup
+    },
+    // callback function to run when the query finishes:
+    (err, rows) => {
+      console.log(rows);
+      if (rows.length > 0) {
+        res.send(rows[0]);
+      } else {
+        res.send({}); // failed, so return an empty object instead of undefined
+      }
+    }
+  );
+});
+
+
+// start the server at URL: http://localhost:3000/
+app.listen(3000, () => {
+  console.log('Server started at http://localhost:3000/');
+});
