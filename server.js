561169b76ca911d3ba8518058419f0cb42d979de
auhndrew <35355959+auhndrew@users.noreply.github.com>
2019-06-02 18:19:18 -0700

    Final Milestone

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+/*
+  All database handling is done here. Our sqlite database is in charge
+  of account handling, as well as the account linking to educator plans.
+*/
 
 const express = require('express');
 const app = express();
@@ -11,61 +15,88 @@ const db = new sqlite3.Database('passwords.db');
 app.use(express.static('static_files'));
 
 
-app.get('/users', (req, res) => {
-  // db.all() fetches all results from an SQL query into the 'rows' variable:
-  db.all('SELECT name FROM users_to_passwords', (err, rows) => {
-    console.log(rows);
-    const allUsernames = rows.map(e => e.name);
-    console.log(allUsernames);
-    res.send(allUsernames);
-  });
-});
-
-app.get('/current', (req, res) => {
-  // db.all() fetches all results from an SQL query into the 'rows' variable:
-  db.all('SELECT * FROM current_user', (err, rows) => {
-    if(rows[0]) {
-      res.send(rows[0]);
-    }
-    else {
-      res.send({});
-    }
-       // failed, so return an empty object instead of undefined
-
-  });
-});
-
-app.get('/plans/:name', (req, res) => {
-  db.all('SELECT * FROM plans WHERE username=$username',
-  {
-    $username: req.params.name,
-  },
-  
-  (err, rows) => {
-    if(rows[0]) {
-      res.send(rows[0]);
-    }
-    else {
-      res.send({});
-    }
+//Get all names for the check function on the home page.
+  app.get('/users', (req, res) => {
+    // db.all() fetches all results from an SQL query into the 'rows' variable:
+    db.all('SELECT name FROM users_to_passwords', (err, rows) => {
+      console.log(rows);
+      const allUsernames = rows.map(e => e.name);
+      console.log(allUsernames);
+      res.send(allUsernames);
+    });
   });
 
-});
+  //Gets all usernames and passwords
+  app.get('/current', (req, res) => {
+    // db.all() fetches all results from an SQL query into the 'rows' variable:
+    db.all('SELECT * FROM current_user', (err, rows) => {
+      if(rows[0]) {
+        res.send(rows[0]);
+      }
+      else {
+        res.send({});
+      }
+        // failed, so return an empty object instead of undefined
 
-app.get('/plans/code/:code', (req, res) => {
-  db.all('SELECT * FROM plans WHERE sharecode=$code',
-  {
-    $code: req.params.code,
-  },
-  (err, rows) => {
-    if(rows[0]) {
-      res.send(rows[0]);
-    }
-    else {
-      res.send({});
-    }
+    });
   });
-});
+
+  //Finds a specific plan according to a certain username
+  app.get('/plans/:name', (req, res) => {
+    db.all('SELECT * FROM plans WHERE username=$username',
+    {
+      $username: req.params.name,
+    },
+    
+    (err, rows) => {
+      if(rows[0]) {
+        res.send(rows[0]);
+      }
+      else {
+        res.send({});
+      }
+    });
+
+  });
+
+  //Finds a specific plan using the educator share code
+  app.get('/plans/code/:code', (req, res) => {
+    db.all('SELECT * FROM plans WHERE sharecode=$code',
+    {
+      $code: req.params.code,
+    },
+    (err, rows) => {
+      if(rows[0]) {
+        res.send(rows[0]);
+      }
+      else {
+        res.send({});
+      }
+    });
+  });
+
+  //Finds username/password from a username
+  app.get('/users/:userid', (req, res) => {
+    const nameToLookup = req.params.userid; // matches ':userid' above
+
+    // db.all() fetches all results from an SQL query into the 'rows' variable:
+    db.all(
+      'SELECT * FROM users_to_passwords WHERE name=$name',
+      // parameters to SQL query:
+      {
+        $name: nameToLookup
+      },
+      // callback function to run when the query finishes:
+      (err, rows) => {
+        console.log(rows);
+        if (rows.length > 0) {
+          res.send(rows[0]);
+        } else {
+          res.send({}); // failed, so return an empty object instead of undefined
+        }
+      }
+      );
+  });
 
 // POST data about a user to insert into the database
 // (note that this will insert duplicate entries!)
@@ -75,9 +106,11 @@ app.get('/plans/code/:code', (req, res) => {
 // use this library to parse HTTP POST requests
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true})); // hook up with your app
+
 app.post('/users', (req, res) => {
   console.log(req.body);
 
+  //Adds a new user
   db.run(
     'INSERT INTO users_to_passwords VALUES ($name, $password, $SorT, $plan)',
     // parameters to SQL query:
@@ -93,6 +126,26 @@ app.post('/users', (req, res) => {
         res.send({message: 'error in app.post(/users)'});
       } else {
         res.send({message: 'successfully run app.post(/users)'});
+      }
+    }
+    );
+});
+
+//Removes a plan based on who is logged in
+app.post('/plans/:name', (req, res) => {
+
+  db.run(
+    'DELETE FROM plans WHERE username=$name',
+    {
+      $name: req.params.name,
+    },
+    // callback function to run when the query finishes:
+    (err) => {
+      if (err) {
+        console.log(err);
+        res.send({message: 'error in app.post(/users/:username)'});
+      } else {
+        res.send({message: 'successfully run app.post(/users/:username)'});
       }
     }
     );
@@ -118,6 +171,7 @@ app.post('/current', (req, res) => {
     );
 });
 
+//Distinguishes between educator and student
 app.post('/SorT', (req, res) => {
   console.log(req.body);
 
@@ -139,6 +193,7 @@ app.post('/SorT', (req, res) => {
     );
 });
 
+//Adds a plan into the database with a unique sharecode
 app.post('/plans', (req, res) => {
   console.log(req.body);
 
@@ -159,52 +214,6 @@ app.post('/plans', (req, res) => {
       }
     }
     );
-});
-app.get('/users/:userid', (req, res) => {
-  const nameToLookup = req.params.userid; // matches ':userid' above
-
-  // db.all() fetches all results from an SQL query into the 'rows' variable:
-  db.all(
-    'SELECT * FROM users_to_passwords WHERE name=$name',
-    // parameters to SQL query:
-    {
-      $name: nameToLookup
-    },
-    // callback function to run when the query finishes:
-    (err, rows) => {
-      console.log(rows);
-      if (rows.length > 0) {
-        res.send(rows[0]);
-      } else {
-        res.send({}); // failed, so return an empty object instead of undefined
-      }
-    }
-    );
-});
-
-
-// GET a list of all galleries
-//
-// To test, open this URL in your browser:
-//   http://localhost:3000/users
-app.get('/galleries', (req, res) => {
-  const allGalleries = Object.keys(fakeGalleries); // returns a list of object keys
-  console.log('fakeGalleries is:', fakeGalleries);
-  res.send(allGalleries);
-});
-
-
-// GET gallery data
-//
-app.get('/galleries/:galleryid', (req, res) => {
-  const galleryToSearch = req.params.galleryid; // matches ':galleryid' above
-  const val = fakeGalleries[galleryToSearch];
-  console.log(galleryToSearch, '->', val); // for debugging
-  if (val) {
-    res.send(val);
-  } else {
-    res.send({}); // failed, so return an empty object instead of undefined
-  }
 });
 
 
